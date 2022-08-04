4f67f311fd838952e52229452f85e12a58cb5d64
brainbox <strony@brainbox.com.pl>
2022-08-04 12:28:21 +0200

    Porządki po integracji deva z gitchubem

--- wp-content/themes/esez-child/single-ewidencjazgonow.php
+++ wp-content/themes/esez-child/single-ewidencjazgonow.php
@@ -5,7 +5,9 @@
  */
 acf_form_head();
 get_header();
-
+if(isset($_POST['_acf_form']) && !empty($_POST['_acf_form'])){
+    echo 'Success!';
+}
 global $post;
 $author_id = $post->post_author;
 $user = get_the_author_meta( 'ID' );
@@ -20,27 +22,172 @@ global $current_user;
 wp_get_current_user();
 $items = array();
 
-foreach( get_coauthors() as $coauthor ) {
+foreach( get_multiple_authors() as $coauthor ) {
      $items[] = $coauthor->ID;
 }
 
-if (is_user_logged_in() && in_array($current_user->ID, $items)) : ?>
+if (is_user_logged_in() && in_array($current_user->ID, $items) || current_user_can('administrator')) : ?>
 <style>
     .esez-container {
-        max-width: 1100px;
+        max-width: 900px;
+        padding: 32px 0;
     }
+
+    .acf-fields {
+        border: 1px solid #E1E1E1;
+    }
+
+    /* pola */
+    #acf-field_627cfd56b1d15,
+    #acf-field_627cf75e7e3f1,
+    #acf-field_6290abafe4b82-field_6298b5e9de204 {
+        text-align: start;
+    }
+
+    #acf-field_627cf75e7e3f1,
+    #acf-field_627cf4e87e3ee,
+    #acf-field_627cfd56b1d15,
+    #acf-field_627cf6c87e3ef,
+    #acf-field_62a1b642910f9,
+    #acf-field_6059d4efa0515,
+    #acf-field_60112e303f973,
+    #acf-field_60112e573f974,
+    #acf-field_6017dfc3d014f,
+    #dp1659535698987,
+    .acf-date-picker,
+    .acf-time-picker,
+    .acf-date-time-picker,
+    #acf-field_6137462544bfd,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295e8f260432>div.acf-input>div>div.acf-field.acf-field-number.acf-field-6295e90b60435.hide-label.-r0>div.acf-input,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295e8f260432>div.acf-input>div>div.acf-field.acf-field-text.acf-field-6295e8f260433.hide-label.-r0>div.acf-input,
+    #acf-field_60097abd5a117,
+    #acf-field_601151613f976,
+    #acf-field_62a1b70830cd5,
+    #acf-field_62cd382a10d31,
+    #acf-field_627cf8307e3f2,
+    #acf-field_627cf8fc7e3f3,
+    #acf-field_601429bd3ccb6-field_601429bd3ccb9,
+    #acf-field_601429bd3ccb6-field_601429bd3ccba,
+    #acf-field_60140a20aeae8-field_60140d77aeaeb,
+    #acf-field_60140a20aeae8-field_6014134caeaec {
+        width: 37.7%;
+    }
+
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-text.acf-field-62a1df2beb547>div.acf-input {
+        width: 13%;
+    }
+
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6290abafe4b82>div.acf-input>div>div.acf-field.acf-field-number.acf-field-6298b5e9de204.hide-label.-r0>div.acf-input,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295e93060436>div.acf-input>div>div.acf-field.acf-field-number.acf-field-6295e93060438.hide-label.-r0>div.acf-input {
+        width: 18%;
+    }
+
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-628e500b88425>div.acf-input>div>div.acf-field.acf-field-text.acf-field-628e501e88426.hide-label.-r0>div.acf-input,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6290a31e922b8>div.acf-input>div>div.acf-field.acf-field-text.acf-field-6290a33e922b9.hide-label.-r0>div.acf-input {
+        width: 30%;
+    }
+
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-60140a20aeae8>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-60140c56aeae9.-r0>div.acf-input>div,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295c733be746>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295c85bebd40.-r0>div.acf-input>div,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295c733be746>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295ca92ebd41.-r0>div.acf-input>div,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295f58acdbfa>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295f58acdbfb.-r0>div.acf-input>div,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295f58acdbfa>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295f58acdbfc.-r0>div.acf-input>div,
+    #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-601429bd3ccb6>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-601429bd3ccb7.-r0>div.acf-input>div {
+        width: 78%;
+    }
+
+    .acf-field input[type=text],
+    .acf-field input[type=password],
+    .acf-field input[type=date],
+    .acf-field input[type=datetime],
+    .acf-field input[type=datetime-local],
+    .acf-field input[type=email],
+    .acf-field input[type=month],
+    .acf-field input[type=number],
+    .acf-field input[type=search],
+    .acf-field input[type=tel],
+    .acf-field input[type=time],
+    .acf-field input[type=url],
+    .acf-field input[type=week],
+    .acf-field textarea,
+    .acf-field select {
+        font-size: 1em !important;
+    }
+
+    @media (max-width: 992px) {
+
+        #acf-field_627cf75e7e3f1,
+        #acf-field_627cf4e87e3ee,
+        #acf-field_627cfd56b1d15,
+        #acf-field_627cf6c87e3ef,
+        #acf-field_62a1b642910f9,
+        #acf-field_6059d4efa0515,
+        #acf-field_60112e303f973,
+        #acf-field_60112e573f974,
+        #acf-field_6017dfc3d014f,
+        #dp1659535698987,
+        .acf-date-picker,
+        .acf-time-picker,
+        #acf-field_6137462544bfd,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295e8f260432>div.acf-input>div>div.acf-field.acf-field-number.acf-field-6295e90b60435.hide-label.-r0>div.acf-input,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295e8f260432>div.acf-input>div>div.acf-field.acf-field-text.acf-field-6295e8f260433.hide-label.-r0>div.acf-input,
+        #acf-field_60097abd5a117,
+        #acf-field_601151613f976,
+        #acf-field_62a1b70830cd5,
+        #acf-field_62cd382a10d31,
+        #acf-field_627cf8307e3f2,
+        #acf-field_627cf8fc7e3f3,
+        #acf-field_601429bd3ccb6-field_601429bd3ccb9,
+        #acf-field_601429bd3ccb6-field_601429bd3ccba,
+        #acf-field_60140a20aeae8-field_60140d77aeaeb,
+        #acf-field_60140a20aeae8-field_6014134caeaec {
+            width: 100%;
+        }
+
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-text.acf-field-62a1df2beb547>div.acf-input {
+            width: 100%;
+        }
+
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6290abafe4b82>div.acf-input>div>div.acf-field.acf-field-number.acf-field-6298b5e9de204.hide-label.-r0>div.acf-input,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295e93060436>div.acf-input>div>div.acf-field.acf-field-number.acf-field-6295e93060438.hide-label.-r0>div.acf-input {
+            width: 100%;
+        }
+
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-628e500b88425>div.acf-input>div>div.acf-field.acf-field-text.acf-field-628e501e88426.hide-label.-r0>div.acf-input,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6290a31e922b8>div.acf-input>div>div.acf-field.acf-field-text.acf-field-6290a33e922b9.hide-label.-r0>div.acf-input {
+            width: 100%;
+        }
+
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-60140a20aeae8>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-60140c56aeae9.-r0>div.acf-input>div,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-60140a20aeae8>div.acf-input>div>div.acf-field.acf-field-time-picker.acf-field-60140ce9aeaea.-r0>div.acf-input>div,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295c733be746>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295c85bebd40.-r0>div.acf-input>div,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295c733be746>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295ca92ebd41.-r0>div.acf-input>div,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295f58acdbfa>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295f58acdbfb.-r0>div.acf-input>div,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-6295f58acdbfa>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-6295f58acdbfc.-r0>div.acf-input>div,
+        #acf-form>div.acf-fields.acf-form-fields.-left>div.acf-field.acf-field-group.acf-field-601429bd3ccb6>div.acf-input>div>div.acf-field.acf-field-date-picker.acf-field-601429bd3ccb7.-r0>div.acf-input>div {
+            width: 100%;
+        }
+    }
+
     @media (min-width: 1640px) {
         .totalcost-container {
-            right: 18rem;
+            right: 32rem;
         }
+
         .drukuj {
-            right: 21.3rem;
+            right: 39rem;
         }
+
         .acf-form-submit input {
-            right: 25.8rem;
+            right: 31.8rem;
         }
+
         .go-to-top {
-            right: 19rem; 
+            right: 29rem;
+        }
+
+        .location-direct {
+            right: 43rem;
         }
     }
 </style>
@@ -50,7 +197,10 @@ if (is_user_logged_in() && in_array($current_user->ID, $items)) : ?>
             <h1 class="esez-title">Numer opaski: <?php the_field('numer_opaski'); ?></h1>
         </header><!-- /.entry-header -->
         <?php
-			acf_form();
+			acf_form(array(
+                'label_placement' => 'left',
+                'updated_message' => __("Zaktualizowano dane!", 'acf'),
+            ));
             if(get_field('uslugi_pogrzebowe')) :
             include('/home/esez/public_html/wp-content/plugins/ewidencja-zmarlych/public/templates/cennik-uslug-pogrzebowych.php');
             endif;
