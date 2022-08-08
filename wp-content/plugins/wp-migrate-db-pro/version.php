dfe35cba3a7ce8ac1e3c6e5954f5142683a7bd67
brainbox <strony@brainbox.com.pl>
2022-08-08 13:29:08 +0200

    Usunięcie EWWW oraz dodanie nowej funkcji bulk

--- wp-content/plugins/wp-migrate-db-pro/version.php
+++
@@ -1,2 +0,0 @@
-<?php
-$GLOBALS['wpmdb_meta']['wp-migrate-db-pro']['version'] = '5.0.0';
--- wp-content/themes/esez-child/single-ewidencjazgonow.php
+++ wp-content/themes/esez-child/single-ewidencjazgonow.php
@@ -228,7 +228,7 @@ if (is_user_logged_in() && in_array($current_user->ID, $items) || current_user_c
             <?php foreach( $images as $image ): ?>
             <a href="<?php echo $image['url']; ?>" data-lightbox="galeria"><img
                     class="single-ewidencjazgonow__gallery__img"
-                    src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" /></a>
+                    src="<?php echo $image['sizes']['ewidencja-zdjecie']; ?>" alt="<?php echo $image['alt']; ?>" /></a>
             <?php endforeach; ?>
 
         </div>
